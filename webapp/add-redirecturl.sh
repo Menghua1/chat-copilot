658275dc12a4917a9e52e3046d129b77614d9883
Menghua Chen (WICRESOFT NORTH AMERICA LTD) <v-menghchen@microsoft.com>
2024-02-02 16:00:41 +0800

    Modify

--- webapp/add-redirecturl.sh
+++ webapp/add-redirecturl.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
  
-  OBJECT_ID=$(az ad app show --id ${frontendClientId} | jq -r '.id')
-  BODY="{spa:{redirectUris:['${webUrl}']}}"
-  az login
+# Add web url to frontend app registration.
+ 
+if ($env:AZURE_FRONTEND_APPLICATION_ID -ne '') {
+  OBJECT_ID=$(az ad app show --id $env:AZURE_FRONTEND_APPLICATION_ID | jq -r '.id')
+  BODY="{spa:{redirectUris:['$env:REACT_APP_WEB_BASE_URL']}}"
+ 
+  echo "Updating redirect with $BODY"
+ 
   az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications/$OBJECT_ID" --headers 'Content-Type=application/json' --body $BODY
+}
