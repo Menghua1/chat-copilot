c39469088f662ff6642eef25df993c408460a69b
Menghua Chen (WICRESOFT NORTH AMERICA LTD) <v-menghchen@microsoft.com>
2024-02-02 14:12:21 +0800

    Modify

--- webapp/add-redirecturl.sh
+++ webapp/add-redirecturl.sh
@@ -2,4 +2,5 @@
  
   OBJECT_ID=$(az ad app show --id ${frontendClientId} | jq -r '.id')
   BODY="{spa:{redirectUris:['${webUrl}']}}"
+  az login
   az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications/$OBJECT_ID" --headers 'Content-Type=application/json' --body $BODY
